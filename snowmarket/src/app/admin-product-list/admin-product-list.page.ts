ca5de9133e27144027e0e0b9c0b9ab165402fcec
KN Tse <overload0715@gmail.com>
2023-09-05 18:19:17 +0800

    image

--- snowmarket/src/app/admin-product-list/admin-product-list.page.ts
+++ snowmarket/src/app/admin-product-list/admin-product-list.page.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductListPage implements OnInit {
   productList = [
-    { id: '1', name: 'apple', price: '$30' },
-    { id: '2', name: 'banana', price: '$12' },
-    { id: '3', name: 'orange', price: '$6' },
-    { id: '4', name: 'cherry', price: '$8' },
-    { id: '5', name: 'mango', price: '$13' },
+    { id: '1', name: 'apple', price: '$30', stock: '999' },
+    { id: '2', name: 'banana', price: '$12', stock: '999' },
+    { id: '3', name: 'orange', price: '$6', stock: '999' },
+    { id: '4', name: 'cherry', price: '$8', stock: '999' },
+    { id: '5', name: 'mango', price: '$13', stock: '999' },
   ];
   constructor() {}
 
